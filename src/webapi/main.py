c77150796f5230e62588b389d10591ef39f595b4
1ntegrale9 <1ntegrale9uation@gmail.com>
2020-10-17 22:17:21 +0900

    [Feat] FastAPIの基本構成を追加

---
+++ src/webapi/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Bot(BaseModel):
+    name: str
+
+
+app = FastAPI(
+    title='Discord Bot Database',
+    docs_url='/'
+)
+
+
+@app.get('/api/bots')
+def get_bots():
+    return {'message': 'Hello World'}
+
+
+@app.get('/api/bots/{id}')
+def get_bot():
+    return {'message': 'Hello World'}
+
+
+@app.post('/api/bots')
+def post_bots(bot: Bot):
+    return {'message': 'Hello World'}
+
+
+@app.put('/api/bots/{id}')
+def put_bots(bot: Bot):
+    return {'message': 'Hello World'}
+
+
+@app.delete('/api/bots')
+def delete_bots():
+    return {'message': 'Hello World'}
